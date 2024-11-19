c37d86afd75768274d2c0ce934148ca77cf5ce31
newmanreb <rebekah.s.newman@gmail.com>
2024-11-19 18:24:31 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#! /home/newmanreb/.conda/envs/PythonProgramming/bin/python
+
+"""
+A simple hello world script!
+"""
+
+
+def print_hi(name):
+    """
+    A simple function that takes a text string variable, and uses it in string formatting which is printed
+    """
+    print(f'Hi, {name}')
+
+
+string = "PyCharm"
+print_hi(string)
